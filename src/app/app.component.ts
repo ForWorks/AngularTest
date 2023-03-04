95fb5c6721b39ed869bf353b2bb43dd4415ac13e
ForWork <vladikgatilo@gmail.com>
2023-03-04 23:32:41 +0300

    Implemented the main UI, added the ability to delete and add users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestTask';
+
+  users: User[] = [];
+  roles: Role[] = [];
+
+  addUser(userName) {
+    // проверка данных, регулярка и защита от html инъекций
+    this.users.push({id: 3, name: userName, roles: []});
+    return false;
+  }
+
+  addRole(userRole) {
+    this.roles.push({id: 3, name: userRole});
+    return false;
+  }
+
+  deleteRole(roleId) {
+    for(let i = 0; i < this.roles.length; i++) {
+      if(this.roles[i].id == roleId) {
+        this.roles.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  deleteUser(userId) {
+    for(let i = 0; i < this.users.length; i++) {
+      if(this.users[i].id == userId) {
+        this.users.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.users.push({id: 0, name: 'Влад', roles: []});
+    this.users.push({id: 1, name: 'Рома', roles: []});
+    this.users.push({id: 2, name: 'Гриша', roles: []});
+
+    this.roles.push({id: 0, name: 'Роль1'});
+    this.roles.push({id: 1, name: 'Роль2'});
+    this.roles.push({id: 2, name: 'Роль3'});
+  }
+}
+
+interface User {
+	id: number,
+	name: string,
+  roles: Role[],
+}
+
+interface Role {
+  id: number,
+  name: string,
 }
